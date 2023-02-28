7c1a41f5af56da031ba6cd4f6077dc8a408ddc71
Gabriel <gabriel.bitten59@gmail.com>
2023-02-27 22:53:16 -0300

    Fix 2

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import HeaderVue from "./components/Header.vue";
+
 
 </script>
 
 
 
 <template>
-  <HeaderVue />
+  
   <RouterView />
 </template>
 
