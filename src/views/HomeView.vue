084b27a566f4596906bdea69c3ce3bc58fd874f4
Gabriel <gabriel.bitten59@gmail.com>
2023-03-14 22:01:43 -0300

    Fix(Home): ajustado responsividade mobile

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,9 +11,9 @@ Loading.isLoading = false;
 
 <template>
 
-    <div class="w-full h-full flex flex-col items-center">
-        <img class="w-[600px] h-[200px] mt-10" src="/Logo.png">
-        <RouterLink @click="Loading.isLoading = true" to="/perfil" class="mt-[10vh] h-[80px] w-[400px] flex justify-center items-center bg-[#4EEE90] text-[#FFFFFF] text-[24px] font-[700] font-lexend tracking-tight">
+    <div class="w-full h-full flex flex-col items-center justify-center xl:justify-start">
+        <img class="w-[470px] xl:w-[600px] h-[140px] xl:h-[200px] mt-0 xl:mt-10" src="/Logo.png">
+        <RouterLink @click="Loading.isLoading = true" to="/perfil" class="mt-6 xl:mt-[10vh] h-[80px] w-[400px] flex justify-center items-center bg-[#4EEE90] text-[#FFFFFF] text-[24px] font-[700] font-lexend tracking-tight">
             Entrar com perfil
         </RouterLink>
     </div>
