ddbfb6df8244a43a44bb44ba1740f069595745b4
Gabriel <gabriel.bitten59@gmail.com>
2023-04-07 19:01:12 -0300

    express test 2

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 require("dotenv").config();
-import express from "express";
-import cors from "cors";
-import axios from "axios";
-const fetch = require('node-fetch');
-const rateLimiter = require('express-rate-limit');
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+//const rateLimiter = require('express-rate-limit');
 
 
 
@@ -12,26 +11,23 @@ app.use(cors({ origin: ['http://localhost:5173','http://localhost:3000'], creden
 const port = 3000;
 
 
-
+/*
 const limiter = rateLimiter({
 
     windowMs: 1000,
     max: 1,
 });
 app.use(limiter);
-
+*/
 
 
 
 // EXPRESS MIDDLEWARE API ROUTES
-app.get('/api/LastPlayed/:id_profile', cors(), (req, res, next) => {
+app.get('/api/LastPlayed/:id_profile', cors(), (req, res) => {
 
     const options = {
         method: "GET",
         url: `https://api.cognicenter.com.br/Atividades.php?educacao=1&target=getLastPlayed&id_profile=${req.params.id_profile}`,
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-        },
     }
     
     axios
@@ -43,7 +39,6 @@ app.get('/api/LastPlayed/:id_profile', cors(), (req, res, next) => {
         res.json(err);
     })
 
-    next();
 });
 
 
